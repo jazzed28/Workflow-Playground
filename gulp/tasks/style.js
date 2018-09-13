7f5459c5bcc2fe30b474089dde4e6aa76caafc76
jazzed28 <peteson.me@gmail.com>
2018-09-13 23:41:43 +1200

    test gulp error handler

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -11,5 +11,9 @@ gulp.task('style', () => {
   console.log('Something is being done with CSS');
   return gulp.src(cssSourcePath)
     .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .on('error', (error) => {
+      console.log(error.toString());
+      this.emit('end');
+    })
     .pipe(gulp.dest('./app/dist/'));
 })
