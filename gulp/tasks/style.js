74dbf299c80e04755bc92019fad75c273f2e2449
jazzed28 <peteson.me@gmail.com>
2018-09-13 23:38:04 +1200

    add browsersync and organize gulp tasks files

---
+++ gulp/tasks/style.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssvars = require('postcss-simple-vars');
+const nested = require('postcss-nested');
+const cssImport = require('postcss-import');
+
+const cssSourcePath = './app/assets/styles/style.css';
+
+gulp.task('style', () => {
+  console.log('Something is being done with CSS');
+  return gulp.src(cssSourcePath)
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/dist/'));
+})
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const browserSync = require('browser-sync').create();
+
+const htmlPath = './app/index.html';
+
+gulp.task('watch', () => {
+
+  browserSync.init({
+    nofify: false,
+    server: {
+      baseDir: "app"
+    }
+  })
+
+  watch(htmlPath, () => {
+    browserSync.reload();
+  })
+
+  watch('./app/assets/styles/**/*.css', () => {
+    gulp.start('cssInject');
+  })
+})
+
+gulp.task('cssInject', ['style'],() => {
+  return gulp.src('./app/dist/style.css')
+    .pipe(browserSync.stream());
+})
