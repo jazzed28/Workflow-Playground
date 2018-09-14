b47d138b8df5857441cbd45870dd8bc3bcf3c5d4
jazzed28 <peteson.me@gmail.com>
2018-09-14 20:12:06 +1200

    mobile-first tests with postcss-maxin and responsive images

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -4,13 +4,14 @@ const autoprefixer = require('autoprefixer');
 const cssvars = require('postcss-simple-vars');
 const nested = require('postcss-nested');
 const cssImport = require('postcss-import');
+const mixins = require('postcss-mixins');
 
 const cssSourcePath = './app/assets/styles/style.css';
 
 gulp.task('style', () => {
   console.log('Something is being done with CSS');
   return gulp.src(cssSourcePath)
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .on('error', (error) => {
       console.log(error.toString());
       this.emit('end');
