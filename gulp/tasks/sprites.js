f6746cb585af5960dea2808f3f3956a8b78076c2
jazzed28 <peteson.me@gmail.com>
2018-09-23 23:56:21 +1200

    add del to remove directories

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
 var rename = require('gulp-rename');
+var del = require('del');
 
 var config = {
   mode: {
@@ -15,21 +16,29 @@ var config = {
   }
 }
 
-gulp.task('createSprite', function() {
+gulp.task('beginClean', function() {
+  return del(['./app/dist/sprite', './app/assets/images/sprites'])
+});
+
+gulp.task('createSprite', ['beginClean'], function() {
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/dist/sprite/'));
 });
 
 gulp.task('copySpriteGraphic', ['createSprite'], function() {
-  return gulp.src('./app/dist/sprite/css/**/*.svg')
+  return gulp.src('./app/dist/sprite/css/*.svg')
     .pipe(gulp.dest('./app/assets/images/sprites'));
-})
+});
 
 gulp.task('copySpriteCSS', ['createSprite'], function() {
   return gulp.src('./app/dist/sprite/css/*.css')
     .pipe(rename('_sprite.css'))
     .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
+  return del('./app/dist/sprite');
 })
 
-gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
